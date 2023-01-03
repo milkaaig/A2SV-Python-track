d85238a469613d7f35947845da464055dab1428a
milkaaig <105880326+milkaaig@users.noreply.github.com>
2023-01-03 20:05:28 +0300

    Create Another One Bites The Dust.py

---
+++ Another One Bites The Dust.py
@@ -0,0 +1,11 @@
+freq = list(input().split())
+a = int(freq[0])
+b = int(freq[1])
+ab = int(freq[2])
+ 
+if a == b:
+    print(a + b + ab+ ab)
+elif a > b:
+    print(b+b+1+ab +ab )
+else:
+    print(a+a+1+ab + ab)
