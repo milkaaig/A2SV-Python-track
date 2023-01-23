49aaebd1c77d1ef312e37e8f4f02dd9197269eb7
milkaaig <105880326+milkaaig@users.noreply.github.com>
2023-01-23 14:04:19 +0300

    Time: 2415 ms (11.74%), Space: 18.9 MB (76.27%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -6,31 +6,14 @@ class Solution:
             a = []
             for j in range(n):
                 a.append(grid[j][i])
-                # print(a)
+               
             arr.append(a)
             
-        # print(arr)
+       
         count = 0
         for row in range(n):
                 for col in range(n):
                     if grid[row] == arr[col]:
                         count += 1
         return count
-        # N=len(grid)
-        # arr=[]
-        # for ii in range(N):
-        #     a=[]
-        #     for i in range(len(grid)):
-        #         for j in range(len(grid[i])):
-        #             if j==ii:
-        #                 a.append(grid[i][j])
-        #                 print(a)
-        #     arr.append(a)
-        #     a=[]
-            # print(arr)
-        # count=0
-        # for xx in range(len(grid)):
-        #     for yy in range(len(arr)):
-        #         if grid[xx]==arr[yy]:
-        #             count+=1
-        # return count
+        
