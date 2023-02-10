bc1b72cd7a185fa03d89ba9306c1d0dfe6ba40b6
milkaaig <105880326+milkaaig@users.noreply.github.com>
2023-02-10 22:25:45 +0300

    Time: 749 ms (83.62%), Space: 27.6 MB (10.65%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,22 +1,22 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         maxarea = 0
-        r = 0
-        l = len(height) - 1
+        l = 0
+        r = len(height) - 1
         
-        while r < l :
+        while l < r :
 #             min because we are forming a container (to no create imbalance of rectangle)
             y = min(height[r] , height[l])
-            x = l - r
+            x = r - l
             area =  x * y
             
             if area > maxarea:
                 maxarea = area
             
-            if height[r] < height[l]:
-                r += 1
+            if height[r] > height[l]:
+                l += 1
             else:
-                l -= 1
+                r -= 1
                 
         return maxarea
             
