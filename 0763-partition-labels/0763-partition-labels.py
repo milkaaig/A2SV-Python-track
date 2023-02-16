4697db8623dee0a772eb418d2cd721299fa5e26c
milkaaig <105880326+milkaaig@users.noreply.github.com>
2023-02-16 21:41:01 +0300

    Time: 44 ms (57.43%), Space: 13.8 MB (97.18%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,22 +1,25 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-#         Painful gruelling so painful
         mapp = {}
         size = len(s)
-        for i in range(size):
+        #last index of char(last seen index)
+        for i in range(len(s)):
             mapp[s[i]] = i
-            
+        
+        
+        #compares lenght of s[i] and e append if i = e and restars the size
         output = []
         l = 0
         r = mapp[s[0]]
         
         for i in range(size):
             r = max(r, mapp[s[i]])
+            
             if i == r:
-                output.append(r - l + 1)
+                leng = r - l + 1
+                output.append(leng)
                 l = r + 1
             
+        
         return output
-        
-        
             
