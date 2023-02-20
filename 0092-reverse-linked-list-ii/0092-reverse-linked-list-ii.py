0e01ef191eb805f9cf4999f751c296067f9fcb39
milkaaig <105880326+milkaaig@users.noreply.github.com>
2023-02-20 23:14:59 +0300

    Time: 34 ms (62.63%), Space: 14.1 MB (44.15%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -5,17 +5,21 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-       
+#        segment to be reversed
+        rev = right - left
+        if  rev == 0:
+            return head
         
         #left side of the segment
-        output = ListNode(0, head)
-        lefty = output
+        outdum = ListNode(0, head)
+        outdum.next = head
+        lefty = outdum
         
         for i in range(left - 1):
             lefty = lefty.next
         
         prev = lefty.next
-        segment = prev
+        segment = lefty.next
         curr = prev.next
         
         l = left
@@ -25,16 +29,13 @@ class Solution:
             prev = curr
             curr = nekst
             l += 1
-          
+        
         lefty.next = prev
         segment.next = curr
        
-        # print(segment)
+        # print(lefty.next)
        
-        # print(output)
-        output = output.next
-        
-        return output
-    
-        
        
+        outdum = outdum.next
+        
+        return outdum
