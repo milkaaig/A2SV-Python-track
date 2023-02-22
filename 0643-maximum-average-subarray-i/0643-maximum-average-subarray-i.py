e596da62be3c444b7d068b2675d0bd654d0c38be
milkaaig <105880326+milkaaig@users.noreply.github.com>
2023-02-22 23:35:28 +0300

    Time: 1400 ms (25.00%), Space: 26.2 MB (14.19%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -14,13 +14,6 @@ class Solution:
             maxav = max(maxav , subav)
             i +=  1
             
-#         def max_subarray_sum(arr, k):
-#          window_sum = sum(arr[:k])
-#          max_sum = window_sum
 
-#          for end in range(k, len(arr)):
-#          window_sum += arr[end]
-#          window_sum -= arr[end-k]
-#          max_sum = max(max_sum, window_sum)
 
         return maxav
