f5f2d459e2a9b6df2311d44c0c7a567193e5164c
milkaaig <105880326+milkaaig@users.noreply.github.com>
2023-03-05 11:41:02 +0300

    Time: 721 ms (92.99%), Space: 39 MB (73.85%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -7,23 +7,23 @@ class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         t = head
         r = head
-        
+        # R goes to the end and t reaches the middle
         while r and r.next:
             t = t.next
             r = r.next.next
-        
+            
         prev = None
-        
+        #Reverse starting from the last position of turtle 
         while t:
             temp = t.next
             t.next = prev
             prev = t
             t = temp
-            
+        
         while prev:
             if head.val != prev.val:
                 return False
             head = head.next
             prev = prev.next
-            
+        
         return True
