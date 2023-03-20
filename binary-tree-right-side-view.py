cc67feffa524985a2f59f2fbb5efbe7a79212d1a
milkaaig <105880326+milkaaig@users.noreply.github.com>
2023-03-20 19:46:29 +0300

    added binary-tree-right-side-view.python

---
+++ binary-tree-right-side-view.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        val = defaultdict(list)
+
+        def traverse(node, depth):
+            if not node:
+                return
+            
+            val[depth] = node.val
+
+            traverse(node.left, depth + 1)
+            traverse(node.right, depth + 1)
+        
+        traverse(root, 0)
+
+        return val.values()
