bd9a02d0c8cccf9d2f06f3c8c5122da7d8dda6a1
milkaaig <105880326+milkaaig@users.noreply.github.com>
2023-04-15 17:04:47 +0300

    added sum-of-nodes-with-even-valued-grandparent.python

--- sum-of-nodes-with-even-valued-grandparent.py
+++ sum-of-nodes-with-even-valued-grandparent.py
@@ -3,35 +3,34 @@
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
-#         self.right = right 
-# ayyi 
+#         self.right = right
 class Solution:
     def sumEvenGrandparent(self, root: TreeNode) -> int:
         output = 0
 
-        def evengran(cur):
-            
-            if not cur:
-                return
-
-            if cur.val % 2 == 0:
-                if cur.left:
-                    sum(cur.left)
-
-                if cur.right:
-                    sum(cur.right)
-
-            evengran(cur.left)
-            evengran(cur.right)
-
-        def sum (node):
+        def checker(node):
             nonlocal output
+            if not node:
+                return
+            
+            if node.val % 2 == 0:
+                if node.left:
+                    if node.left.left:
+                        output += node.left.left.val
+                    if node.left.right:
+                        output += node.left.right.val
+                    
+                if node.right:
+                    if node.right.right:
+                        output += node.right.right.val
+                    if node.right.left:
+                        output += node.right.left.val
+                    
 
             if node.left:
-                output += node.left.val
+                checker(node.left)
             if node.right:
-                output += node.right.val
-
-        evengran(root)
-
+                checker(node.right)
+        
+        checker(root)
         return output
