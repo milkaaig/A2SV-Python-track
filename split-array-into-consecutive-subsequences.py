5ea42aa01e09df5113bc8db4b020beae13f2f33b
milkaaig <105880326+milkaaig@users.noreply.github.com>
2023-07-28 18:14:59 +0300

    added split-array-into-consecutive-subsequences.python

--- split-array-into-consecutive-subsequences.py
+++ split-array-into-consecutive-subsequences.py
@@ -4,14 +4,15 @@ class Solution:
 
         for n in nums:
             if dic[n - 1]:
+                # it is basically checking lenght of the subsequence before the current number and then adding that length to the current one removing the previous. it keeps the length of consecutive subsequences
                 heappush(dic[n], heappop(dic[n - 1]) + 1)
+                
             else:
                 heappush(dic[n], 1)
-
+        
         for lst in  dic.values():
             for i in lst:
                 if i < 3:
                     return False
-            # if sum(lst) < len(lst) * 3 :
-            #     return False
+            
         return True
