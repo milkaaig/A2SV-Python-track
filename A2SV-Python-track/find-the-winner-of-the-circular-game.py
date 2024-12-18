cbff3b960a10f8454759e017f8446be7be5caff7
milkaaig <105880326+milkaaig@users.noreply.github.com>
2024-12-18 21:48:05 +0300

    Added solution for Find the Winner of the Circular Game

---
+++ A2SV-Python-track/find-the-winner-of-the-circular-game.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        circle = list(range(1, n + 1))
+        
+        i = 0
+        
+        while len(circle) > 1:
+            i = (i + k - 1)% len(circle)
+            circle.pop(i)
+        
+        return circle[0]
+        
